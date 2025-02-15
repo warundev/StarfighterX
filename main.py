2479b89e74db7cf396b0f7fd9a3470aedeb3337c
warundev <120333797+warundev@users.noreply.github.com>
2025-02-15 19:22:20 +0530

    2nd update

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ pygame.init()
  # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# title and icon
+pygame.display.set_caption("StarfighterX")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
 running  = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+
+  screen.fill((0, 0, 0))
+

279118a26653cd56408c1accff8346f9f706b6bc
warundev <120333797+warundev@users.noreply.github.com>
2025-02-16 15:46:08 +0530

    player and monster add done

--- main.py
+++ main.py
@@ -11,11 +11,53 @@ pygame.display.set_caption("StarfighterX")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# enemy
+enemyImg = pygame.image.load('monster.png')
+enemyX = 370
+enemyY = 50
+enemyX_change = 0
+
+def player(x,y):
+  screen.blit(playerImg, (x, y))
+
+def enemy(x,y):
+  screen.blit(enemyImg, (x, y))
+
 running  = True
 while running:
+
+  screen.fill((0, 0, 0))
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  screen.fill((0, 0, 0))
+    # moving the player
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        playerX_change = -0.1
+      if event.key == pygame.K_RIGHT:
+        playerX_change = 0.1
+
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        playerX_change = 0
+
+
+  playerX += playerX_change
+
+  if playerX <= 0:
+    playerX = 0
+  elif playerX >= 736:
+    playerX = 736
+
+  player(playerX,playerY)
+  enemy(enemyX, enemyY)
+  pygame.display.update()
 

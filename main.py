3821582310008a6565700f308e71c2ae5a2f9d49
warundev <120333797+warundev@users.noreply.github.com>
2025-02-17 15:05:22 +0530

    background set

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 # initialize pygame
@@ -5,6 +7,10 @@ pygame.init()
 
  # create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# background
+background = pygame.image.load('background.png')
+
 
 # title and icon
 pygame.display.set_caption("StarfighterX")
@@ -19,9 +25,10 @@ playerX_change = 0
 
 # enemy
 enemyImg = pygame.image.load('monster.png')
-enemyX = 370
-enemyY = 50
-enemyX_change = 0
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 2.5
+enemyY_change = 40
 
 def player(x,y):
   screen.blit(playerImg, (x, y))
@@ -29,10 +36,12 @@ def player(x,y):
 def enemy(x,y):
   screen.blit(enemyImg, (x, y))
 
+# game loop
 running  = True
 while running:
 
   screen.fill((0, 0, 0))
+  screen.blit(background, (0, 0))
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -41,10 +50,9 @@ while running:
     # moving the player
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT:
-        playerX_change = -0.1
+        playerX_change = -5
       if event.key == pygame.K_RIGHT:
-        playerX_change = 0.1
-
+        playerX_change = 5
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
         playerX_change = 0
@@ -56,6 +64,15 @@ while running:
     playerX = 0
   elif playerX >= 736:
     playerX = 736
+
+  enemyX += enemyX_change
+
+  if enemyX <= 0:
+    enemyX_change = 3
+    enemyY += enemyY_change
+  elif enemyX >= 736:
+    enemyX_change = -3
+    enemyY += enemyY_change
 
   player(playerX,playerY)
   enemy(enemyX, enemyY)
